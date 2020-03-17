781c13c864ea3a5f6738d48d829cd4379566c8c7
Srikant Sarwa <ssarwa@microsoft.com>
2020-03-17 13:07:30 -0700

    Add project files.

---
+++ Models/MyDatabaseContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace firstclose_demo.Models
+{
+    public class MyDatabaseContext: DbContext
+    {
+        public MyDatabaseContext() : base("name=MyDbConnection")
+        {
+        }
+
+        public System.Data.Entity.DbSet<firstclose_demo.Models.Todo> Todoes { get; set; }
+    }
+}
---
+++ Models/Todo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace firstclose_demo.Models
+{
+    public class Todo
+    {
+        public int ID { get; set; }
+        public string Description { get; set; }
+
+        [Display(Name = "Created Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime CreatedDate { get; set; }
+    }
+}
